e827be54370cfe16bbe9cf80f4b9acfb491e1be6
saikrishna446 <66399465+saikrishna446@users.noreply.github.com>
2022-07-06 13:31:34 +0530

    adding file

---
+++ src/main/java/com/qentelli/automation/testdatafactory/data/CustomerType.java
@@ -0,0 +1,24 @@
+package com.qentelli.automation.testdatafactory.data;
+
+public enum CustomerType {
+        FreeUser( "REGISTEREDUSER"),
+        CoachUser( "REGISTEREDUSER,COACH"),
+        ClubUser( "REGISTEREDUSER,CLUB"),
+        ClubCoachUser("REGISTEREDUSER,CLUB,COACH"),
+        PreferredCustomer("REGISTEREDUSER,PC"),
+        ClubPreferredCustomer("REGISTEREDUSER,CLUB,PC");
+
+
+
+        private final String custString;
+        private String test;
+
+        CustomerType(String custString){
+            this.custString = custString;
+        }
+
+        public String getCustString(){
+            return custString;
+        }
+
+}
